51eaa028c38d4f52b49d2a519a3e3d38971dc3d1
KTYDG <74158983+KTYDG@users.noreply.github.com>
2022-07-07 09:55:46 +0300

    Delete functions/target directory

--- functions/target/debug/deps/functions.d
+++
@@ -1,5 +0,0 @@
-C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\functions\target\debug\deps\functions.exe: src\main.rs
-
-C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\functions\target\debug\deps\functions.d: src\main.rs
-
-src\main.rs:
--- functions/target/debug/functions.d
+++
@@ -1,1 +0,0 @@
-C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\functions\target\debug\functions.exe: C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\functions\src\main.rs
