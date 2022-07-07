993d30c3fdb41cd3704e297a42f71d1801b213b7
KTYDG <74158983+KTYDG@users.noreply.github.com>
2022-07-07 09:56:04 +0300

    Delete branches/target directory

--- branches/target/debug/branches.d
+++
@@ -1,1 +0,0 @@
-C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\branches\target\debug\branches.exe: C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\branches\src\main.rs
--- branches/target/debug/deps/branches.d
+++
@@ -1,5 +0,0 @@
-C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\branches\target\debug\deps\branches.exe: src\main.rs
-
-C:\Users\molok\OneDrive\Документы\GitHub\HelpMeIAmRUUUSTED\branches\target\debug\deps\branches.d: src\main.rs
-
-src\main.rs:
